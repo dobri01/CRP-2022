f4aa925d6a4bfb167d42f49a107811bcb3b2b8cb
tla097 <tomarmstrong131@gmail.com>
2022-10-18 14:13:07 +0100

    semi working cloud update

--- src/pf_localisation/pf.py
+++ src/pf_localisation/pf.py
@@ -1,15 +1,15 @@
 from geometry_msgs.msg import Pose, PoseArray, Quaternion
-from . pf_base import PFLocaliserBase
+from .pf_base import PFLocaliserBase
 import math
 import rospy
 
-from . util import rotateQuaternion, getHeading
+from .util import rotateQuaternion, getHeading
 import random
 
 from time import time
 
 
-#test push with pycharm
+# test push with pycharm
 
 class PFLocaliser(PFLocaliserBase):
 
@@ -25,23 +25,18 @@ class PFLocaliser(PFLocaliserBase):
             self.ODOM_DRIFT_NOISE = ???? # Odometry model y axis (side-to-side) noise
         """
 
-        self.ODOM_ROTATION_NOISE = 1 # Odometry model rotation noise
-        self.ODOM_TRANSLATION_NOISE = 1 # Odometry model x axis (forward) noise
-        self.ODOM_DRIFT_NOISE = 1 # Odometry model y axis (side-to-side) noise
-
+        self.ODOM_ROTATION_NOISE = 0.05  # Odometry model rotation noise
+        self.ODOM_TRANSLATION_NOISE = 0.05  # Odometry model x axis (forward) noise
+        self.ODOM_DRIFT_NOISE = 0.05 # Odometry model y axis (side-to-side) noise
 
         # ----- Sensor model parameters
-        self.NUMBER_PREDICTED_READINGS = 20     # Number of readings to predict
-
-
+        self.NUMBER_PREDICTED_READINGS = 20  # Number of readings to predict
 
     # NIAM + TOM
     def initialise_particle_cloud(self, initialpose):
 
-
         # return the dots
         # map is occupancy_map a grid of all locations
-
 
         # how do we spread it out on a map we don't know
 
@@ -67,13 +62,12 @@ class PFLocaliser(PFLocaliserBase):
 
         """
         Set particle cloud to initialpose plus noise
-        
 
         Called whenever an initialpose message is received (to change the
         starting location of the robot), or a new occupancy_map is received.
         self.particlecloud can be initialised here. Initial pose of the robot
         is also set here.
-        
+
         :Args:
             | initialpose: the initial pose estimate
         :Return:
@@ -96,65 +90,136 @@ class PFLocaliser(PFLocaliserBase):
         #
         # return self.particlecloud
 
-
-
     # JOSH
     def update_particle_cloud(self, scan):
-    
-    #samples w/ pose in self.particlecloud
-    #get weight of each pose - self.sensor_model.get_weight()
-    #resample w/ new poses (resampling wheel)
-    #add noise to new samples
-    #replace old cloud w/ new one.
-    
-    #Resampling wheel:
-        print(self.particlecloud)
-        S2 = []
-        weights = self.sensor_model.get_weight(self.particlecloud)
-        M = len(self.particlecloud)
-        u = 0
-        index = random.randint(1,M)
-        while len(S2) < len(self.particlecloud):
-            u = u + random.uniform(0,2*max(weights))
-            while weights[index] < u: # while u > weight of current index
-                u = u - weights[index]
-                index = (index + 1)
-            S2.append(self.particlecloud[index])
-        self.particlecloud = S2
-        print(self.particlecloud)
-        """
-        This should use the supplied laser scan to update the current
-        particle cloud. i.e. self.particlecloud should be updated.
 
-        :Args:
-            | scan (sensor_msgs.msg.LaserScan): laser scan to use for update
+        #    # samples w/ pose in self.particlecloud
+        #    # get weight of each pose - self.sensor_model.get_weight()
+        #    # resample w/ new poses (resampling wheel)
+        #    # add noise to new samples
+        #    # replace old cloud w/ new one.
+        #    """
+        #    #Resampling wheel:
+        #
+        #
+        # print(self.particlecloud)
+        #    self.particlecloud.poses
+        #    new_particle_cloud = []
+        #
+        #    #to get the individual poses weights
+        #
+        #    amount_of_poses = len(self.particlecloud.poses)
+        #
+        #    weights = []
+        #    for i in range(amount_of_poses):
+        #        pose = self.particlecloud.poses[i]
+        #        weights.append(self.sensor_model.get_weight(pose))
+        #
+        #
+        #    # self.sensor_model.get_weight(scan[i], pose[i]) compares scan to the pose
+        #
+        #
+        #
+        #    u = 0
+        #    index = random.randint(1,amount_of_poses)
+        #    while len(new_particle_cloud) < amount_of_poses:
+        #        u = u + random.uniform(0,2*max(weights))
+        #        while weights[index] < u: # while u > weight of current index
+        #            u = u - weights[index]
+        #            index = (index + 1)
+        #        new_particle_cloud.append(self.particlecloud[index])
+        #    self.particlecloud = new_particle_cloud
+        #    print(self.particlecloud)
+        #    """
+        #
+        # systematic resampling algorithm
 
-         """
+
+        amount_of_poses = len(self.particlecloud.poses)
+        commulutive_weights = []
+
+        weights = []
+
+        for i in range(amount_of_poses):
+           pose = self.particlecloud.poses[i]
+           weights.append(self.sensor_model.get_weight(scan, pose))
+
+        commulutive_weights.append(weights[0])
+        for x in range(1, self.NUMBER_PREDICTED_READINGS):
+           commulutive_weights.append(commulutive_weights[x - 1] + weights[x])
+
+        threshold = random.uniform(0, 1 / self.NUMBER_PREDICTED_READINGS)
+
+        i = 0
 
 
+        cloud_to_return = []
+
+
+        for count in range(0, self.NUMBER_PREDICTED_READINGS):
+           while threshold > commulutive_weights[i]:
+               i = i + 1
+           # add noise
+           self.particlecloud.poses[i].position.x = random.gauss(self.particlecloud.poses[i].position.x, (
+                       self.particlecloud.poses[i].position.x * self.ODOM_DRIFT_NOISE))
+           self.particlecloud.poses[i].position.y = random.gauss(self.particlecloud.poses[i].position.y, (
+                       self.particlecloud.poses[i].position.y * self.ODOM_TRANSLATION_NOISE))
+           self.particlecloud.poses[i].orientation = rotateQuaternion(self.particlecloud.poses[i].orientation,
+                                                                      math.radians(
+                                                                          random.uniform(-self.ODOM_ROTATION_NOISE,
+                                                                                         self.ODOM_ROTATION_NOISE)))
+
+           cloud_to_return.append(self.particlecloud.poses[i])
+           threshold = threshold + 1 / self.NUMBER_PREDICTED_READINGS
+
+        self.particlecloud.poses = cloud_to_return
+
+        #
+        # # samples w/ pose in self.particlecloud
+        # # get weight of each pose - self.sensor_model.get_weight()
+        # # resample w/ new poses (resampling wheel)
+        # # add noise to new samples
+        # # replace old cloud w/ new one.
+        #
+        # # Resampling wheel:
+        # print(self.particlecloud)
+        # S2 = []
+        # weights = self.sensor_model.get_weight(self.particlecloud)
+        # M = len(self.particlecloud)
+        # u = 0
+        # index = random.randint(1, M)
+        # while len(S2) < len(self.particlecloud):
+        #     u = u + random.uniform(0, 2 * max(weights))
+        #     while weights[index] < u:  # while u > weight of current index
+        #         u = u - weights[index]
+        #         index = (index + 1)
+        #     S2.append(self.particlecloud[index])
+        # self.particlecloud = S2
+        # print(self.particlecloud)
+        # """
+        # This should use the supplied laser scan to update the current
+        # particle cloud. i.e. self.particlecloud should be updated.
+        # :Args:
+        #     | scan (sensor_msgs.msg.LaserScan): laser scan to use for update
+        #  """
 
     # DOBRI
     def estimate_pose(self):
         """
         This should calculate and return an updated robot pose estimate based
         on the particle cloud (self.particlecloud).
-
         Create new estimated pose, given particle cloud
         E.g. just average the location and orientation values of each of
         the particles and return this.
-
         Better approximations could be made by doing some simple clustering,
         e.g. taking the average location of half the particles after
         throwing away any which are outliers
-
         :Return:
             | (geometry_msgs.msg.Pose) robot's estimated pose.
          """
 
         pass
 
-        # result = Pose()
-        #
-        # return result
+        result = Pose()
 
-
+        return result

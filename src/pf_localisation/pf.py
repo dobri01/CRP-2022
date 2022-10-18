ed3e4da15886f5570562096c6359175920ae65f5
tla097 <tomarmstrong131@gmail.com>
2022-10-18 01:05:49 +0100

    fix syntax errors in estimate pose function

--- src/pf_localisation/pf.py
+++ src/pf_localisation/pf.py
@@ -108,20 +108,20 @@ class PFLocaliser(PFLocaliserBase):
     #replace old cloud w/ new one.
     
     #Resampling wheel:
-    	print(self.particlecloud)
-    	S2 = []
-    	weights = self.sensor_model.get_weight(self.particlecloud)
-    	M = len(self.particlecloud)
-    	u = 0
-    	index = random.randint(1,M)
-    	while len(S2) < len(self.particlecloud)
-    		u = u + random.uniform(0,2*max(weights))
-    		while weights[index] < u # while u > weight of current index
-    			u = u - weights[index]
-    			index = (index + 1)
-    		S2.append(self.particlecloud[index])
-    	self.particlecloud = S2
-    	print(self.particlecloud)
+        print(self.particlecloud)
+        S2 = []
+        weights = self.sensor_model.get_weight(self.particlecloud)
+        M = len(self.particlecloud)
+        u = 0
+        index = random.randint(1,M)
+        while len(S2) < len(self.particlecloud):
+            u = u + random.uniform(0,2*max(weights))
+            while weights[index] < u: # while u > weight of current index
+                u = u - weights[index]
+                index = (index + 1)
+            S2.append(self.particlecloud[index])
+        self.particlecloud = S2
+        print(self.particlecloud)
         """
         This should use the supplied laser scan to update the current
         particle cloud. i.e. self.particlecloud should be updated.
@@ -131,7 +131,6 @@ class PFLocaliser(PFLocaliserBase):
 
          """
 
-        return []
 
 
     # DOBRI
@@ -152,9 +151,10 @@ class PFLocaliser(PFLocaliserBase):
             | (geometry_msgs.msg.Pose) robot's estimated pose.
          """
 
+        pass
 
-        result = Pose()
-
-        return result
+        # result = Pose()
+        #
+        # return result
 
 

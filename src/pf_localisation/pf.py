0bdc2d17654b836453fead25dd021a0613bdae8a
tla097 <tomarmstrong131@gmail.com>
2022-10-19 15:01:31 +0100

    completely fixed the repeated updating issue

--- src/pf_localisation/pf.py
+++ src/pf_localisation/pf.py
@@ -9,10 +9,19 @@ import random
 
 import time
 
+import numpy.ma as ma
+
+from geometry_msgs.msg import Twist
+
+
+
+
+
 
 # test push with pycharm
 
 class PFLocaliser(PFLocaliserBase):
+    moved = False
 
     def __init__(self):
         # ----- Call the superclass constructor
@@ -26,12 +35,12 @@ class PFLocaliser(PFLocaliserBase):
             self.ODOM_DRIFT_NOISE = ???? # Odometry model y axis (side-to-side) noise
         """
 
-        self.ODOM_ROTATION_NOISE = 45
-        self.ODOM_TRANSLATION_NOISE = 1
-        self.ODOM_DRIFT_NOISE = 1
+        self.ODOM_ROTATION_NOISE = 5
+        self.ODOM_TRANSLATION_NOISE = 0.04
+        self.ODOM_DRIFT_NOISE = 0.04
 
         # ----- Sensor model parameters
-        self.NUMBER_PREDICTED_READINGS = 20  # Number of readings to predict
+        self.NUMBER_PREDICTED_READINGS = 100  # Number of readings to predict
 
     # NIAM + TOM
     def initialise_particle_cloud(self, initialpose):
@@ -42,7 +51,7 @@ class PFLocaliser(PFLocaliserBase):
         # how do we spread it out on a map we don't know
 
         # for now set the noise/ standard deviation to 1
-        noise = 1.3
+        noise = 1
 
         poseArray = PoseArray()
 
@@ -93,8 +102,20 @@ class PFLocaliser(PFLocaliserBase):
 
     # JOSH
 
+    def callback(self, data):
+        self.display("calshshhshhshlback")
+        PFLocaliser.moved = True
+
+
+    def listener(self):
+        rospy.wait_for_message("/cmd_vel", Twist, 10)
+        # rospy.Subscriber("/cmd_vel", Twist, self.callback)
+        self.display("in listener")
+
 
     def update_particle_cloud(self, scan):
+        self.display("about to enter listener")
+        self.listener()
 
         # self.display("Called particle filter")
         # #
@@ -146,17 +167,21 @@ class PFLocaliser(PFLocaliserBase):
         #
         # systematic resampling algorithm
 
-        amount_of_poses = len(self.particlecloud.poses)
+
+        cloud = PoseArray()
+        cloud = self.particlecloud
+        amount_of_poses = len(cloud.poses)
         commulutive_weights = []
 
         weights = []
 
+
         for i in range(amount_of_poses):
-            pose = self.particlecloud.poses[i]
+            pose = cloud.poses[i]
             weights.append(self.sensor_model.get_weight(scan, pose))
 
-        self.display("WEIGHTS")
-        self.display(weights)
+        # self.display("WEIGHTS")
+        # self.display(weights)
 
         commulutive_weights.append(weights[0])
         for x in range(1, self.NUMBER_PREDICTED_READINGS):
@@ -173,11 +198,11 @@ class PFLocaliser(PFLocaliserBase):
                 i = i + 1
             # add noise
             noisy_pose = Pose()
-            noisy_pose.position.x = random.gauss(self.particlecloud.poses[i].position.x,
-                    (self.particlecloud.poses[i].position.x * self.ODOM_DRIFT_NOISE))
-            noisy_pose.position.y = random.gauss(self.particlecloud.poses[i].position.y,
-                    (self.particlecloud.poses[i].position.y * self.ODOM_TRANSLATION_NOISE))
-            noisy_pose.orientation = rotateQuaternion(self.particlecloud.poses[i].orientation,
+            noisy_pose.position.x = random.gauss(cloud.poses[i].position.x,
+                    (cloud.poses[i].position.x * self.ODOM_DRIFT_NOISE))
+            noisy_pose.position.y = random.gauss(cloud.poses[i].position.y,
+                    (cloud.poses[i].position.y * self.ODOM_TRANSLATION_NOISE))
+            noisy_pose.orientation = rotateQuaternion(cloud.poses[i].orientation,
                     math.radians(random.uniform(-self.ODOM_ROTATION_NOISE, self.ODOM_ROTATION_NOISE)))
 
             poses_to_return.append(noisy_pose)
@@ -189,45 +214,90 @@ class PFLocaliser(PFLocaliserBase):
         # self.display(cloud_to_return)
         # self.display("\n\n\n\n\n\n\n\n\n\n\n\n\n")
 
-
+        # self.display(poses_to_return)
         cloud_to_return = PoseArray()
         cloud_to_return.poses.extend(poses_to_return)
 
         self.particlecloud = cloud_to_return
 
 
+    def update_particle_cloud2(self, scan):
+
+        ##---------------------------##
+        ##---------PARAMETERs--------##
+        ##---------------------------##
+        #list of poses
+        sum_weights = 0
+        sum_count = 0
+        cumulate_weight= []
+        separate_weight= []
+
+        cloud_pose = self.particlecloud.poses
+
+        ##---------------------------##
+        ##-----------WEIGHT----------##
+        ##---------------------------##
+
+        ##remove the invalid value.
+        scan.ranges=ma.masked_invalid(scan.ranges).filled(scan.range_max)
+
+        ##record the weight for each pose, in case to use it again
+        pairs_partweight=[]
+        #time1 = datetime.datetime.now()
+
+        ##weight need scan_data and current_pose
+        for i in cloud_pose:
+            particle_weight = self.sensor_model.get_weight(scan, i)
+            sum_weights+= particle_weight
+            pairs_partweight.append([i,particle_weight])
+        #time2 = datetime.datetime.now()
+        ##calculate the time consume
+        #print (time2 - time1 ).microseconds
+
+        for pair in pairs_partweight:
+            particle_weight = pair[1]
+            weight_over_sum = particle_weight/sum_weights
+            sum_count+= weight_over_sum
+            cumulate_weight.extend([sum_count])
+
+        #print cumulate_weight
+        ##-----------------------##
+        ##---------UPDATE--------##
+        ##-----------------------##
+        updated_particlecloud = PoseArray()
+        for particle in cloud_pose:
+            count = 0
+            rand = random.uniform(0,1)
+            for i in cumulate_weight:
+                ##TODO:the repeat pose doesn't matters
+                if rand <= i:
+                    updated_particlecloud.poses.extend([cloud_pose[count]])
+                    #print count
+                    break
+                count=count+1
+
+        ##-----------------------##
+        ##---------NOISE---------##
+        ##-----------------------##
+
+        updated_with_noise_cloud = PoseArray()
+
+        for i in updated_particlecloud.poses:
+            noise_pose = Pose()
+            noise_pose.position.x = random.gauss(i.position.x,(i.position.x * self.ODOM_DRIFT_NOISE))
+            noise_pose.position.y = random.gauss(i.position.y,(i.position.y * self.ODOM_TRANSLATION_NOISE))
+            noise_pose.orientation = rotateQuaternion(i.orientation, math.radians(random.uniform(-self.ODOM_ROTATION_NOISE,self.ODOM_ROTATION_NOISE)))
+
+            updated_with_noise_cloud.poses.extend([noise_pose])
+
+        ##-----------------------##
+        ##---------OUTPUT--------##
+        ##-----------------------##
+
+        self.particlecloud = updated_with_noise_cloud
+
     def display(self, message):
         rospy.loginfo(message)
-
-
-        #
-        # # samples w/ pose in self.particlecloud
-        # # get weight of each pose - self.sensor_model.get_weight()
-        # # resample w/ new poses (resampling wheel)
-        # # add noise to new samples
-        # # replace old cloud w/ new one.
-        #
-        # # Resampling wheel:
-        # print(self.particlecloud)
-        # S2 = []
-        # weights = self.sensor_model.get_weight(self.particlecloud)
-        # M = len(self.particlecloud)
-        # u = 0
-        # index = random.randint(1, M)
-        # while len(S2) < len(self.particlecloud):
-        #     u = u + random.uniform(0, 2 * max(weights))
-        #     while weights[index] < u:  # while u > weight of current index
-        #         u = u - weights[index]
-        #         index = (index + 1)
-        #     S2.append(self.particlecloud[index])
-        # self.particlecloud = S2
-        # print(self.particlecloud)
-        # """
-        # This should use the supplied laser scan to update the current
-        # particle cloud. i.e. self.particlecloud should be updated.
-        # :Args:
-        #     | scan (sensor_msgs.msg.LaserScan): laser scan to use for update
-        #  """
 
         # DOBRI
     def estimate_pose(self):
